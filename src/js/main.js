1da0437812a272607f5a8460621e258262219969
Thefeli73 <thefeli73@hotmail.com>
2020-01-15 18:08:42 +0100

    add wrapper, eventlistner, css optimize

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+document.getElementById("menuToggle").addEventListener("click", menuToggle);
 function menuToggle() {
   var bars = document.getElementsByClassName('bar');
   for (var i = 0; i < bars.length; i++) {
