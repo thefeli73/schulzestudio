31ca8d8a1272840e5b5db9a1f99e9b77580343d0
Felix Schulze <thefeli73@hotmail.com>
2020-02-22 16:58:55 +0100

    add jquery and truly fix menu link
    fix so menu links arent clickable while menu is hidden

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
 //toggle menu burger animation and menu
 document.getElementById("menuToggle").addEventListener("mousedown", menuToggle);
 function menuToggle() {
-  document.getElementById("menu").classList.toggle("show"); //shows menu
-  var bars = document.getElementsByClassName('bar');
+  //removes menu if disabled and brings it back if enabled
+  if ($("#menu").css('display') != 'block') {
+    $("#menu").css('display','block'); //enables menu
+    setTimeout(function(){
+      document.getElementById("menu").classList.toggle("show"); //reveals menu
+    },100);
+  } else {
+    document.getElementById("menu").classList.toggle("show"); //hides menu
+    setTimeout(function(){
+      $("#menu").css('display','none'); //disables menu
+    },400);
+  }
+
+  var bars = $('.bar');
   for (var i = 0; i < bars.length; i++) {
     bars[i].classList.toggle('active');
   }
